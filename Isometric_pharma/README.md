d7120388ae0e8fd861c734f3f4f0e6cc215fc6d5
sadimohammad65@gmail.com <sadimohammad65@gmail.com>
2022-09-06 11:18:02 +0600

    Laravel Project

---
+++ Isometric_pharma/README.md
@@ -0,0 +1,31 @@
+# Isometric Pharma Online Pharmacies 
+
+## Type of user: 1. Admin, 2. Sales Man, 3. User/Consumer, 4. Delivery Man
+***Common features for all users:***
+* All users can log in to the system
+* All users can Registration in the system
+* CRUD operation
+
+***Features of Admin:***
+*	Manage the overall system
+*	Access and show all information of every user
+*	Approve salesman & Delivery Man
+
+***Features of Sells Man:***
+*	Admin will approve new seller 
+*	Cancel the order if the customer did not pay
+*	Can see delivery status
+*	Process an Order
+*	Contact Admin, Deliveryman & Customer
+
+***Features of User/Consumer:***
+*	Search Medicine
+*	Can place an order
+*	Payment on a card system
+*	Review & Rating
+*	Access Order & Delivery history
+
+***Features of Delivery Man:***
+*	Take a shipment order
+*	Order delivery, Contact the customer, can give delivery status 
+*	Delivery & payment history
---
+++ Isometric_pharma/resources/views/pages/order/allOrderListStatusPieChart.blade.php
@@ -0,0 +1,76 @@
+<html>
+  <head>
+  <title>Order Status Chart</title>
+
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+ 
+
+
+
+      function drawChart() {
+
+        var data = google.visualization.arrayToDataTable([
+          ['Task', 'Hours per Day'],
+        <?php echo $orderStatusPieChartData?>
+        ]);
+        
+
+        var options = {
+          title: 'Order Status Chart',
+          pieHole: 0.4,
+         
+          
+        
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById("piechart_3d"));
+        chart.draw(data, options);
+        
+        
+      }
+    </script>
+    <style>
+         body {
+            max-width: 100%;
+            overflow-x: hidden;
+        }
+
+    </style>
+  </head>
+
+
+@extends('../../layouts.app')
+@section('content')
+
+<div class="row">
+  
+<div class="col-3" style="min-height: 88vh; background-image: linear-gradient(45deg,  #3D0000,#0b006e)">
+        <div>
+            @if(session('role') == 'admin')
+            @include('pages.admin.adminSideBar')
+            @elseif(session('role') == 'seller')
+            @include('pages.seller.sellerSideBar')
+            @elseif(session('role') == 'service')
+            @include('pages.serviceProvider.serviceProviderSideBar')
+
+            @endif
+        </div>
+       
+    </div>
+  
+  <body>
+    <div id="piechart_3d" style="width: 900px; height: 500px;"></div>
+    
+    
+  </body>
+ 
+
+</div>
+  
+  @endsection
+  
+</html>
+
